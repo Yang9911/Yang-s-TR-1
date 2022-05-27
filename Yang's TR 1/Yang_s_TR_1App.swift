b52145961f7265b09d600429ade72d71951f99c1
Chia-Hao Yang <yang9911@cres.tc.edu.tw>
2022-05-27 16:43:45 +0800

    Initial Commit

---
+++ Yang's TR 1/Yang_s_TR_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Yang_s_TR_1App.swift
+//  Yang's TR 1
+//
+//  Created by Chia-Hao Yang on 2022/5/27.
+//
+
+import SwiftUI
+
+@main
+struct Yang_s_TR_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1ae3707ac6191eb9435c0a4da5834a4f61ad5bd1
CHYang <105098836+Yang9911@users.noreply.github.com>
2022-06-02 20:24:01 +0800

    Add files via upload

--- mathescapeview.swift
+++ mathescapeview.swift
@@ -14,26 +14,26 @@ struct mathescapeview: View {
         ScrollView{
             VStack{
                 Label("逃出數學密室", systemImage: "figure.walk").font(.largeTitle).padding()
-        Text("以下關卡，全部的答案都是阿拉伯數字或英文\n準備好了嗎？\n按下按鈕就開始囉！")
+                Text("以下關卡，全部的答案都是阿拉伯數字或英文\n準備好了嗎？\n按下按鈕就開始囉！")
                     .multilineTextAlignment(.center).padding()
-        Button {
+                Button {
                     ismathshow = true
                     mathnum = 1
                 }label: {
                     Text("開始闖關")
                 }.font(.title).padding(.horizontal,6).padding(.vertical,3).background(Color(.orange)).foregroundColor(.blue).cornerRadius(20)
                 
-        }
+            }
             
         }.overlay(Color.black.opacity(ismathshow ? 1 : 0))
-        .overlay(alignment: .top){mathstage}.animation(.spring(), value: ismathshow)
+            .overlay(alignment: .top){mathstage}.animation(.spring(), value: ismathshow)
     }
     var mathstage:some View {
         Group {
             if (ismathshow) {
                 
                 mathshowview(ismathshow :$ismathshow).transition(.slide).padding()
-
+                
             }
         }
     }

4e1615db85f608d0c1effd08b8440501c71067b6
CHYang <105098836+Yang9911@users.noreply.github.com>
2022-06-04 18:53:58 +0800

    Add files via upload

--- showgame.swift
+++ showgame.swift
@@ -6,36 +6,42 @@
 //
 
 import SwiftUI
-
+import ConfettiSwiftUI
 struct showgame: View {
     @Binding var isonshow:Bool
+//    @State private var counter = 0
     @State var ans = ""
     @State var res = ""
     @State var btn = "送出答案"
     @State private var ispresented = false
     var body: some View {
         ScrollView {
-        VStack {
-            
-            if incorrect > 6{
-                Image("final").resizable().scaledToFill()
+            VStack {
                 
+                if incorrect > 6{
+                    Image("final").resizable().scaledToFill()
+                    
                     TextField("這裡輸入答案", text: $ans).foregroundColor(.red).background(.gray).font(.largeTitle)
                     
-                Button{
-                    ispresented = true
-                    if  ans == "我的老師真是好神" {
-                        res = "恭喜你答對了，全部過關"
-                        btn = ""
-                    }else {
-                        res = "答案不對，再想想看！"
+                    Button{
                         ispresented = true
-                    }
+                        if  ans == "我的老師真是好神" {
+                            res = "恭喜你答對了，全部過關"
+                            btn = ""
+                            ans = ""
+//                            counter = 3
+                                
+                        } else {
+                            res = "答案不對，再想想看！"
+                            ans = ""
+                            ispresented = true
+//                            counter = 0
+                        }
+                        
+                    }label: {
+                        Text(btn)
+                    }.font(.title).background(Color(.white)).foregroundColor(.blue).cornerRadius(20)
                     
-                }label: {
-                    Text(btn)
-                }.font(.title).background(Color(.white)).foregroundColor(.blue).cornerRadius(20)
-                
                     HStack{
                         Image("G1").resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
                         Image("G2").resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
@@ -52,56 +58,66 @@ struct showgame: View {
                         Image("G7").resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
                         Image("G8").resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
                     }
-                
-            } else {
-                Image("t\(tonumber)-2").resizable().scaledToFill()
-            
-                TextField("這裡輸入答案", text: $ans).foregroundColor(.red).background(.gray).font(.title).padding()
-  
-            Button{
-                ans = String.lowercased(ans)()
-                ispresented = true
-                if answer[tonumber] == ans && tonumber == 5 {
-                    imagearray[tonumber] = "G\(tonumber)"
-                    imagearray[tonumber+1] = "G\(tonumber+1)"
-                    incorrect += 1
-                    res = "答對了，請選擇下一題"
-                    btn = ""
-                } else if answer[tonumber] == ans && tonumber == 6 {
-                    imagearray[tonumber+1] = "G\(tonumber+1)"
-                    imagearray[tonumber+2] = "G\(tonumber+2)"
-                    incorrect += 1
-                    res = "答對了，請選擇下一題"
-                    btn = ""
-                } else if answer[tonumber] == ans {
-                    imagearray[tonumber] = "G\(tonumber)"
-                    incorrect += 1
-                    res = "答對了，請選擇下一題"
-                    btn = ""
-                }else {
-                    res = "答案不對，再想想看！"
-                    ans = ""
+                    
+                } else {
+                    Image("t\(tonumber)-2").resizable().scaledToFill()
+                    
+                    TextField("這裡輸入答案", text: $ans).foregroundColor(.red).background(.gray).font(.title).padding()
+                    
+                    Button{
+                        ans = String.lowercased(ans)()
+                        ispresented = true
+                        if answer[tonumber] == ans && tonumber == 5 {
+                            imagearray[tonumber] = "G\(tonumber)"
+                            imagearray[tonumber+1] = "G\(tonumber+1)"
+                            incorrect += 1
+                            res = "答對了，請選擇下一題"
+                            btn = ""
+                            ans = ""
+//                            counter = 5
+                        } else if answer[tonumber] == ans && tonumber == 6 {
+                            imagearray[tonumber+1] = "G\(tonumber+1)"
+                            imagearray[tonumber+2] = "G\(tonumber+2)"
+                            incorrect += 1
+                            res = "答對了，請選擇下一題"
+                            btn = ""
+                            ans = ""
+//                            counter = 6
+                        } else if answer[tonumber] == ans {
+                            imagearray[tonumber] = "G\(tonumber)"
+                            incorrect += 1
+                            res = "答對了，請選擇下一題"
+                            btn = ""
+                            ans = ""
+//                            counter = 8
+                        }else {
+                            res = "答案不對，再想想看！"
+                            ans = ""
+//                            counter = 0
+                        }
+                        
+                    }label: {
+                        Text(btn)
+                    }.font(.title).background(Color(.white)).foregroundColor(.blue).cornerRadius(20)
                 }
                 
-            }label: {
-                Text(btn)
-            }.font(.title).background(Color(.white)).foregroundColor(.blue).cornerRadius(20)
+                Text("按這裡返回").font(.title).padding(.horizontal,6).padding(.vertical,3).foregroundColor(.blue).background(Color(.white)).cornerRadius(20).onTapGesture {
+                    isonshow = false
+                    
+                }
+            }.alert(isPresented: $ispresented){
+                Alert(
+                    title: Text(res)
+                )
             }
+
             
-            Text("按這裡返回").font(.title).padding(.horizontal,6).padding(.vertical,3).foregroundColor(.blue).background(Color(.white)).cornerRadius(20).onTapGesture {
-                isonshow = false
-                
-            }
-        }.alert(isPresented: $ispresented){
-            Alert(
-                title: Text(res)
-            )
         }
-            
-        }.onAppear(){
+        .onAppear(){
             ans = ""
             res = ""
         }
+//        .confettiCannon(counter: $counter, num: 100)
     }
     
     
--- tacoview.swift
+++ tacoview.swift
@@ -15,103 +15,103 @@ struct tacogameview: View {
     @State var isonshow = false
     
     var body: some View {
-    ScrollView{
-        VStack{
-            HStack {
-            Text("章魚遊戲").font(.largeTitle)
-            Image("tacologo").resizable().scaledToFit().frame(width: 100, height: 100, alignment: .center)
+        ScrollView{
+            VStack{
+                HStack {
+                    Text("章魚遊戲").font(.largeTitle)
+                    Image("tacologo").resizable().scaledToFit().frame(width: 100, height: 100, alignment: .center)
+                }
+                Text("下面總共六個關卡，通過六個關卡可以收集八張圖卡，用八張圖卡通過最後一關，即完成本遊戲任務").font(.caption)
+                if incorrect >= 6{
+                    Button{
+                        tonumber = 7
+                        isonshow = true
+                        incorrect += 1
                     }
-            Text("下面總共六個關卡，通過六個關卡可以收集八張圖卡，用八張圖卡通過最後一關，即完成本遊戲任務").font(.caption)
-        if incorrect >= 6{
-            Button{
-                tonumber = 7
-                isonshow = true
-                incorrect += 1
-                          }
-            label:{
-                Text("請前往最後一關").font(.largeTitle)
+                label:{
+                    Text("請前往最後一關").font(.largeTitle)
+                }
+                }
+                //        第二排
+                HStack {
+                    Button{
+                        tonumber = 1
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[1]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    
+                }
+                    Button{
+                        tonumber = 2
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[2]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                }
+                }
+                //第三排
+                HStack {
+                    Button{
+                        tonumber = 3
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[3]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    
+                }
+                    Button{
+                        tonumber = 4
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[4]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                }
+                }
+                //第四排
+                HStack {
+                    Button{
+                        tonumber = 5
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[5]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    
+                }
+                    
+                    if imagearray[6] != "0" {
+                        Image(imagearray[6]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    }
+                }
+                //第五排
+                HStack {
+                    Button{
+                        tonumber = 6
+                        isonshow = true
+                    }
+                label:{
+                    Image(imagearray[7]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    
+                }
+                    
+                    if imagearray[8] != "0" {
+                        Image(imagearray[8]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
+                    }
+                }
             }
-        }
-//        第二排
-        HStack {
-            Button{
-                tonumber = 1
-                isonshow = true
-                          }
-            label:{
-                Image(imagearray[1]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-                
-            }
-        Button{
-            tonumber = 2
-            isonshow = true
-                        }
-        label:{
-            Image(imagearray[2]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-        }
-        }
-        //第三排
-        HStack {
-            Button{
-                tonumber = 3
-                isonshow = true
-                            }
-            label:{
-                Image(imagearray[3]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-                
-            }
-        Button{
-            tonumber = 4
-            isonshow = true
-                        }
-        label:{
-            Image(imagearray[4]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-        }
-        }
-        //第四排
-        HStack {
-            Button{
-                tonumber = 5
-                isonshow = true
-                            }
-            label:{
-                Image(imagearray[5]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-                
-            }
-        
-            if imagearray[6] != "0" {
-            Image(imagearray[6]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-            }
-        }
-        //第五排
-        HStack {
-            Button{
-                tonumber = 6
-                isonshow = true
-                            }
-            label:{
-                Image(imagearray[7]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-                
-            }
-        
-            if imagearray[8] != "0" {
-            Image(imagearray[8]).resizable().scaledToFit().frame(width: 150, height: 100, alignment: .center)
-            }
-        }
-        }
-    }.padding().overlay(Color.black.opacity(isonshow ? 0.8 : 0)).overlay(alignment: .top){preshow}.animation(.spring(), value: isonshow)//scroll
+        }.padding().overlay(Color.black.opacity(isonshow ? 0.8 : 0)).overlay(alignment: .top){preshow}.animation(.spring(), value: isonshow)//scroll
     }
-
-var preshow: some View{
-    Group{
-        
-        if (isonshow) {
-            showgame(isonshow :$isonshow).transition(.slide).padding()
-
+    
+    var preshow: some View{
+        Group{
+            
+            if (isonshow) {
+                showgame(isonshow :$isonshow).transition(.slide).padding()
+                
+            }
+            
         }
-
     }
-}
     
 }
 struct tacogameview_Previews: PreviewProvider {
